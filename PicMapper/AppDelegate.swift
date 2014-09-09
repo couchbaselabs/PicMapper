95e16312a3ed0761a13ea5585fa339fe1d213c42
Chris Anderson <jchris@couchbase.com>
2014-09-09 11:58:15 -0700

    lists all located images

--- PicMapper/AppDelegate.swift
+++ PicMapper/AppDelegate.swift
@@ -7,18 +7,58 @@
 //
 
 import UIKit
+import MapKit
+import AssetsLibrary
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-                            
+    
     var window: UIWindow?
 
 
     func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
         // Override point for customization after application launch.
+        importPhotos()
         return true
     }
 
+    func importPhotos() {
+        let assetsLibrary = ALAssetsLibrary()
+        var groups = [ALAssetsGroup]()
+
+        assetsLibrary.enumerateGroupsWithTypes(0xFFFFFFFF,
+            usingBlock: {(group:ALAssetsGroup!, stop) in
+//                groups.append(group);
+                if group != nil {
+                    NSLog("group %@", group!)
+                    
+                    group.enumerateAssetsUsingBlock {
+                        (asset, index, stop) in
+                        if asset != nil {
+                            let location: AnyObject? = asset.valueForProperty(ALAssetPropertyLocation)
+                            if location.description != "nil" {
+                                NSLog("CLLocation %@", location? as CLLocation)
+                                self.saveLocation(asset)
+                            }
+                        }
+                    }
+                }
+            }, failureBlock: {(error:NSError!) in
+                NSLog("error %@", error)
+            })
+    }
+    
+    func saveLocation(asset: ALAsset) {
+        let location: AnyObject? = asset.valueForProperty(ALAssetPropertyLocation) as CLLocation
+        let rep = asset.defaultRepresentation()
+        let url = rep.url()
+        //        let thumb = asset.thumbnail()
+        NSLog("CLLocation %@", location? as CLLocation)
+        NSLog("URL %@", url)
+        
+
+    }
+    
     func applicationWillResignActive(application: UIApplication!) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
